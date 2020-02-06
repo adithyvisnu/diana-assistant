957cb2dbef099b02eff35040021217f9a54e3563
aminudin <amin4udin@gmail.com>
2020-02-06 17:09:03 +0700

    add helper

--- routes/asisstant.js
+++ routes/asisstant.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const QiscusRequest = require('../utils/helpers/helper');
 
 /* GET home page. */
-router.post('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.post('/', async function(req, res, next) {
+  const request = await QiscusRequest.sendQiscus();
+  console.log(req)
+  console.log(request)
+  res.send(request)
 });
 
 module.exports = router;
---
+++ utils/helpers/helper.js
@@ -0,0 +1,52 @@
+const rp = require('request-promise');
+
+const sendQiscus = async (data) => {
+
+  const uri = `https://api.qiscus.com/api/v2.1/rest/post_comment`;
+  const options = {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'QISCUS-SDK-APP-ID': 'lucinta-a-glhzm4uglkx',
+        'QISCUS-SDK-SECRET': '39c265885f87b74a2c65db9a9989cc7b'
+    },
+    body : {
+        "user_id": "guest-101",
+        "room_id": "9832314",
+        "type": "buttons",
+        "payload": {
+          "text": "silahkan pencet",
+          "buttons": [
+              {
+                  "label": "button1",
+                  "type": "postback",
+                  "payload": {
+                      "url": "http://somewhere.com/button1",
+                      "method": "get",
+                      "payload": null
+                  }
+              },
+              {
+                  "label": "button2",
+                  "type": "link",
+                  "payload": {
+                      "url": "http://somewhere.com/button2?id=123"
+                  }
+              }
+          ]
+        }
+      },
+    uri,
+    json: true
+  };
+  const res = rp(options).then(res => {
+    return res;
+  }).catch((err) => {
+    return err;
+  });
+  return res;
+};
+
+module.exports = {
+    sendQiscus
+};
